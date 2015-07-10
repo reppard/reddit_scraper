8b3c130da9143594672d1db3ca5c77f9a46dc7c8
reppard <reppardwalker@gmail.com>
2015-07-10 01:53:31 +0000

    feat(*): super lazy commit.  who know's whats here

--- scraper.rb
+++ scraper.rb
@@ -1,21 +1,38 @@
+#!/usr/local/bin/ruby
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-SENTENCE_FILE = "sentences.txt"
+SENTENCE_FILE = "www/sentences.txt"
+LOGFILE = "www/scraper.log"
 REDDIT_URL = "https://www.reddit.com/r/"
 SUBREDDITS = [
   "Showerthoughts/",
 ]
 
-data = IO.readlines(SENTENCE_FILE)
-file = File.open(SENTENCE_FILE, 'a')
+while true do
+	data = IO.readlines(SENTENCE_FILE)
+	file = File.open(SENTENCE_FILE, 'a')
+	log  = File.open(LOGFILE, 'a')
 
-page = Nokogiri::HTML(open("#{REDDIT_URL}#{SUBREDDITS[0]}"))
-posts = page.css('a').select{ |a| a["class"] =~ /title/ }
+	log.write("#{Time.now} Getting page...\n")
+	page = Nokogiri::HTML(open("#{REDDIT_URL}#{SUBREDDITS[0]}"))
 
-posts.each do |post|
-  unless data.include? "#{post.text}\n"
-    file.write( "#{post.text}\n" )
-  end
+	log.write("#{Time.now} Getting posts...\n")
+	posts = page.css('a').select{ |a| a["class"] =~ /title/ }
+
+	log.write("#{Time.now} Pushing to file...\n")
+	posts = page.css('a').select{ |a| a["class"] =~ /title/ }
+
+	posts.each do |post|
+	unless data.include? "#{post.text}\n"
+	file.write( "#{post.text}\n" )
+	end
+	end
+
+	log.write("#{Time.now} Done.\n")
+
+	file.close
+	log.close
+	sleep(900)
 end
