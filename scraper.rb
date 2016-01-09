123cb1e834b70a13f1170be50fa4a8041208e325
reppard <reppardwalker@gmail.com>
2016-01-09 20:30:25 +0000

    style(scraper.rb): whitespace

--- scraper.rb
+++ scraper.rb
@@ -11,28 +11,28 @@ SUBREDDITS = [
 ]
 
 while true do
-	data = IO.readlines(SENTENCE_FILE)
-	file = File.open(SENTENCE_FILE, 'a')
-	log  = File.open(LOGFILE, 'a')
+  data = IO.readlines(SENTENCE_FILE)
+  file = File.open(SENTENCE_FILE, 'a')
+  log  = File.open(LOGFILE, 'a')
 
-	log.write("#{Time.now} Getting page...\n")
-	page = Nokogiri::HTML(open("#{REDDIT_URL}#{SUBREDDITS[0]}"))
+  log.write("#{Time.now} Getting page...\n")
+  page = Nokogiri::HTML(open("#{REDDIT_URL}#{SUBREDDITS[0]}"))
 
-	log.write("#{Time.now} Getting posts...\n")
-	posts = page.css('a').select{ |a| a["class"] =~ /title/ }
+  log.write("#{Time.now} Getting posts...\n")
+  posts = page.css('a').select{ |a| a["class"] =~ /title/ }
 
-	log.write("#{Time.now} Pushing to file...\n")
-	posts = page.css('a').select{ |a| a["class"] =~ /title/ }
+  log.write("#{Time.now} Pushing to file...\n")
+  posts = page.css('a').select{ |a| a["class"] =~ /title/ }
 
-	posts.each do |post|
-	unless data.include? "#{post.text}\n"
-	file.write( "#{post.text}\n" )
-	end
-	end
+  posts.each do |post|
+    unless data.include? "#{post.text}\n"
+      file.write( "#{post.text}\n" )
+    end
+  end
 
-	log.write("#{Time.now} Done.\n")
+  log.write("#{Time.now} Done.\n")
 
-	file.close
-	log.close
-	sleep(900)
+  file.close
+  log.close
+  sleep(900)
 end
